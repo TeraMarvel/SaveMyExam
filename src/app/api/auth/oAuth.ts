bde19912b4bd68c4930cfa4a799e58d481376bdc
slark39 <sanyscott4139@gmail.com>
2025-05-24 19:11:18 -0700

    'feat:oauths'

--- src/app/api/auth/oAuth.ts
+++ src/app/api/auth/oAuth.ts
@@ -5,12 +5,11 @@ import GoogleProvider from "next-auth/providers/google";
 export const authOptions = {
   providers: [
     GoogleProvider({
-      clientId:
-        "827096498155-g7uoigrgid0j976a3oa6ua6h66aupntp.apps.googleusercontent.com",
-      clientSecret: "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",
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
   ],
-  secret: "ZTeDUFz8bv7c2GSkG9L5iQ4SoIerL9Qt0flffETbF2M=",
+  secret: process.env.NEXTAUTH_SECRET,
 };
 
 export const { handlers, auth, signIn, signOut } = NextAuth(authOptions);
